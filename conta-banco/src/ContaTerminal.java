2d8c70b7ae2f8a0d47d7e9b09ff8a4f2f2a18e0d
Edgard da Cunha Pontes <edgard.pontes@serpro.gov.br>
2024-05-15 13:21:21 -0300

    Adicionando validação try-catch.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -3,26 +3,30 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         // Conhecer e Importar a classe Scanner
-        @SuppressWarnings("resource")
         Scanner scanner = new Scanner(System.in);
-        
-        // Instanciar a classe Conta
-        Conta conta = new Conta();
+        try {
+            // Instanciar a classe Conta
+            Conta conta = new Conta();
 
-        // Exibir as mensagem para o nosso usuário e Obter pela classe Scanner os valores no terminal
-        System.out.println("Por favor, digite o número da Agência: ");
-        conta.setAgencia(scanner.next());
+            // Exibir as mensagem para o nosso usuário e Obter pela classe Scanner os valores no terminal
+            System.out.println("Por favor, digite o número da Agência: ");
+            conta.setAgencia(scanner.next());
 
-        System.out.println("Por favor, digite o número da Conta: ");
-        conta.setNumConta(scanner.nextInt());
+            System.out.println("Por favor, digite o número da Conta: ");
+            conta.setNumConta(scanner.nextInt());
 
-        System.out.println("Por favor, digite o nome do Cliente: ");
-        conta.setNomeCliente(scanner.next());
+            System.out.println("Por favor, digite o nome do Cliente: ");
+            conta.setNomeCliente(scanner.next());
 
-        System.out.println("Por favor, digite o saldo da Conta: ");
-        conta.setSaldo(scanner.nextDouble());
+            System.out.println("Por favor, digite o saldo da Conta: ");
+            conta.setSaldo(scanner.nextDouble());
 
-        // Exibir a mensagem da conta criada
-        conta.mensagem();
+            // Exibir a mensagem da conta criada
+            conta.mensagem();
+        } catch (Exception e) {
+            System.out.println("Erro ao criar a conta: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
     }
 }
